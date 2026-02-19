fa2efd484f9bacf3d6889b34057310188c51764e
lowerc4s3 <kkirill03@gmail.com>
2026-02-19 13:44:57 +0300

    refactor: move leap.nvim from extraPlugins to lazy.plugins

--- modules/editor/leap.nix
+++ modules/editor/leap.nix
@@ -6,6 +6,7 @@
   }: let
     inherit (lib.nvim.binds) mkKeymap;
     inherit (nvxlib.lua) mkFunc mkLambda;
+    inherit (lib.nvim.dag) entryBefore;
     remoteTextObject = mkFunc "remoteTextObject" ({prefix}: ''
       local ok, ch = pcall(vim.fn.getcharstr) -- pcall for handling <C-c>
       if not ok or (ch == vim.keycode('<esc>')) then return end
@@ -13,15 +14,18 @@
     '');
   in {
     vim.startPlugins = ["vim-repeat"];
-    vim.extraPlugins.leap-nvim = {
+    vim.luaConfigRC.leap-nvim =
+      entryBefore ["mappings"]
+      remoteTextObject.definition;
+    vim.lazy.plugins.leap-nvim = {
       package = "leap-nvim";
-      setup = remoteTextObject.definition;
+      lazy = false; # leap.nvim lazy loads itself
+      keys = [
+        (mkKeymap ["n" "x" "o"] "s" "<Plug>(leap)" {})
+        (mkKeymap "n" "S" "<Plug>(leap-from-window)" {})
+        (mkKeymap ["x" "o"] "ar" (remoteTextObject.lambda ["a"]) {lua = true;})
+        (mkKeymap ["x" "o"] "ir" (remoteTextObject.lambda ["i"]) {lua = true;})
+      ];
     };
-    vim.keymaps = [
-      (mkKeymap ["n" "x" "o"] "s" "<Plug>(leap)" {})
-      (mkKeymap "n" "S" "<Plug>(leap-from-window)" {})
-      (mkKeymap ["x" "o"] "ar" (remoteTextObject.lambda ["a"]) {lua = true;})
-      (mkKeymap ["x" "o"] "ir" (remoteTextObject.lambda ["i"]) {lua = true;})
-    ];
   };
 }
