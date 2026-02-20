c90ed88d3eccc904443886fe8f3fa1fc6edfbc58
lowerc4s3 <kkirill03@gmail.com>
2026-02-21 01:51:00 +0300

    feat: lsp mappings

--- modules/coding/lsp.nix
+++ modules/coding/lsp.nix
@@ -1,8 +1,21 @@
 {
-  flake.modules.nvf.lsp = {
+  flake.modules.nvf.lsp = {options, ...}: {
     vim.lsp = {
       enable = true;
       formatOnSave = true;
+
+      # NOTE: mappings follow neovim's default lsp mapping scheme ('gr' prefix)
+      mappings = let
+        # remove all nvf's default lsp mappings
+        resetDefaults = options.vim.lsp.mappings |> builtins.mapAttrs (_: _: null);
+      in
+        resetDefaults
+        // {
+          goToDefinition = "grd";
+          goToDeclaration = "grD";
+          format = "grf";
+          hover = "K";
+        };
     };
   };
 }
