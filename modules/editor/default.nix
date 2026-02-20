e1d6b55753be90c29c02c5ba3025913b5e45107a
lowerc4s3 <kkirill03@gmail.com>
2026-02-20 13:19:48 +0300

    style: flatten table

--- modules/editor/default.nix
+++ modules/editor/default.nix
@@ -1,12 +1,10 @@
 {self, ...}: {
-  flake.modules.nvf.editor = {
-    imports = with self.modules.nvf; [
-      leap
-      langinput
-      surround
-      autopairs
-      treesitter
-      ts-textobjects
-    ];
-  };
+  flake.modules.nvf.editor.imports = with self.modules.nvf; [
+    leap
+    langinput
+    surround
+    autopairs
+    treesitter
+    ts-textobjects
+  ];
 }
