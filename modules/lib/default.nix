5cb21b57f8f4dbed45c292a02476c909c65e3007
lowerc4s3 <kkirill03@gmail.com>
2026-02-24 21:07:38 +0300

    fix: telescope config and mappings not applying

--- modules/lib/default.nix
+++ modules/lib/default.nix
@@ -6,5 +6,12 @@
 
   config.flake.lib = {
     mkAutocmd = events: opts: opts // {event = lib.toList events;};
+
+    # returns nixpkgs' plugin without dependencies. exists as a workaround for
+    # duplicate plugins in /start and /opt.
+    withoutDeps = pkg:
+      pkg.overrideAttrs {
+        dependencies = [];
+      };
   };
 }
--- modules/ui/telescope.nix
+++ modules/ui/telescope.nix
@@ -7,6 +7,7 @@
   }: let
     inherit (lib.generators) mkLuaInline;
     inherit (lib.nvim.binds) mkKeymap;
+    inherit (lib.nvx) withoutDeps;
   in {
     vim.telescope = {
       enable = true;
@@ -25,11 +26,13 @@
           prompt_position = "top";
           preview_width = 0.5;
         };
-      };
 
-      # leave the prompt by pressing esc
-      setupOpts.mappings.i = {
-        "<esc>" = mkLuaInline ''require("telescope.actions").close'';
+        # leave the prompt by pressing esc
+        mappings.i = {
+          "<esc>" = mkLuaInline ''require("telescope.actions").close'';
+          "<C-j>" = mkLuaInline ''require("telescope.actions").move_selection_next'';
+          "<C-k>" = mkLuaInline ''require("telescope.actions").move_selection_previous'';
+        };
       };
 
       mappings = options.vim.telescope.mappings |> builtins.mapAttrs (_: _: null);
@@ -37,7 +40,7 @@
       extensions = [
         {
           name = "fzf";
-          packages = [pkgs.vimPlugins.telescope-fzf-native-nvim];
+          packages = [(withoutDeps pkgs.vimPlugins.telescope-fzf-native-nvim)];
         }
         # {
         #   name = "ui-select";
@@ -48,11 +51,22 @@
     };
     vim.keymaps = [
       (mkKeymap "n" "<leader>ff" "<cmd>Telescope find_files<cr>" {desc = "Search files";})
+      (mkKeymap "n" "<leader><leader>" "<cmd>Telescope find_files<cr>" {desc = "Search files";})
       (mkKeymap "n" "<leader>fr" "<cmd>Telescope oldfiles<cr>" {desc = "Search recents";})
       (mkKeymap "n" "<leader>fb" "<cmd>Telescope buffers<cr>" {desc = "Search buffers";})
-      (mkKeymap "n" "<leader>fg" "<cmd>Telescope live_grep<cr>" {desc = "Grep files";})
+      (mkKeymap "n" "<leader>fw" "<cmd>Telescope live_grep<cr>" {desc = "Grep files";})
+
       # TODO: check for telescope and project.nvim
       # (mkKeymap "n" "<leader>fp" "<cmd>Telescope projects<cr>" {desc = "Search projects";})
     ];
+    vim.highlight = {
+      TelescopeNormal.link = "FloatBorder";
+      TelescopePromptNormal.link = "CursorLine";
+      TelescopePromptBorder.link = "CursorLine";
+      TelescopePreview.link = "FloatBorder";
+      TelescopePreviewBorder.link = "FloatBorder";
+      TelescopeResults.link = "FloatBorder";
+      TelescopeResultsBorder.link = "FloatBorder";
+    };
   };
 }
