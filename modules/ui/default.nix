04c9926bd676fed1f00218aee5216379f2c86efd
lowerc4s3 <kkirill03@gmail.com>
2026-02-24 23:13:27 +0300

    feat: theme

--- modules/ui/default.nix
+++ modules/ui/default.nix
@@ -1,6 +1,14 @@
 {self, ...}: {
-  flake.modules.nvf.ui.imports = with self.modules.nvf; [
-    icons
-    telescope
-  ];
+  flake.modules.nvf.ui = {
+    vim.theme = {
+      enable = true;
+      style = "dark";
+      name = "oxocarbon";
+    };
+
+    imports = with self.modules.nvf; [
+      icons
+      telescope
+    ];
+  };
 }
